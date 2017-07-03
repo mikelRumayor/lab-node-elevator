e97f5705067d93d8b7f6c1fdad57c108befec368
mikelRumayor <mikel.rumayor@gmail.com>
2017-07-03 17:22:04 +0200

    iteration 1

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,27 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = null;
+    this.intervalId  = null;
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.intervalId = setInterval(() => {this.update()}, 1 * 1000)
+  }
+  stop() {
+    clearInterval(this.intervalId)
+  }
+  update() {
+    this.log()
+  }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() {
+    console.log(`Direction: $(this.direction) | Floor: $(this.floor)`)
+  }
 }
 
 module.exports = Elevator;
