13ce7c08e88a8144846776e0d26e4b84b3ee14a1
mikelRumayor <mikel.rumayor@gmail.com>
2017-07-03 23:22:19 +0200

    iteration 4 almost done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,10 +3,12 @@ const DOWN = 'DOWN'
 
 class Elevator {
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
-    this.intervalId  = null;
+    this.floor      = 0
+    this.MAXFLOOR   = 10
+    this.requests   = []
+    this.waitingList  = []
+    this.passengers = []
+    this.intervalId  = null
     this.direction = null
   }
 
@@ -19,28 +21,36 @@ class Elevator {
   update() {
     this.log()
   }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp()
-  {
+  _passengersEnter() {
+    this.passengers = this.passengers.concat(this.waitingList.filter(e => e.originFloor == this.floor))
+    this.waitingList = this.waitingList.filter(e => e.originFloor != this.floor )
+  }
+  _passengersLeave() {
+    this.passengers = this.passengers.filter(e => e.destinationFloor != this.floor);
+  }
+  floorUp() {
     if (this.floor < this.MAXFLOOR) {
       this.floor++
       this.direction = UP
-      this.log()
+      this.log(console.log(`Direction: ${this.direction} | Floor: ${this.floor}`))
     }
   }
   floorDown() {
-      if (this.floor > 0) {
-        this.floor--
-        this.direction = DOWN
-        this.log()
+    if (this.floor > 0) {
+      this.floor--
+      this.direction = DOWN
+      this.log(console.log(`Direction: ${this.direction} | Floor: ${this.floor}`))
     }
   }
   call(person) {
-    this.requests = [...this.requests, person.originFloor]
+    if( this.passengers.indexOf(person) === -1 ) {
+      this.requests = [...this.requests, person.originFloor]
+      this.waitingList = [...this.waitingList, person]
+    }
+
   }
-  log(){
-    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`)
+  log(callback){
+    callback()
   }
 }
 
