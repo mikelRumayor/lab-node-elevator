32783b20b9cbc62480e432f083ba8512be0aef8c
mikelRumayor <mikel.rumayor@gmail.com>
2017-07-03 17:35:25 +0200

    iteration 2

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,10 +1,13 @@
+const UP = 'UP'
+const DOWN = 'DOWN'
+
 class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
-    this.direction  = null;
     this.intervalId  = null;
+    this.direction = null
   }
 
   start() {
@@ -18,11 +21,24 @@ class Elevator {
   }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
+  floorUp()
+  {
+    if (this.floor < this.MAXFLOOR) {
+      this.floor++
+      this.direction = UP
+      this.log()
+    }
+  }
+  floorDown() {
+      if (this.floor > 0) {
+        this.floor--
+        this.direction = DOWN
+        this.log()
+    }
+  }
   call() { }
-  log() {
-    console.log(`Direction: $(this.direction) | Floor: $(this.floor)`)
+  log(){
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`)
   }
 }
 
